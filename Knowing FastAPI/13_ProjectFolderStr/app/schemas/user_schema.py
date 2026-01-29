c40947370f6dab3b7a6d0c17fec9c31eba52f814
sriram-itspe <sriram.yerra@itspe.co.in>
2026-01-29 15:31:42 +0530

    project str

---
+++ Knowing FastAPI/13_ProjectFolderStr/app/schemas/user_schema.py
@@ -0,0 +1,29 @@
+from sqlmodel import SQLModel
+from pydantic import BaseModel, EmailStr 
+from sqlmodel import SQLModel, Field
+from typing import Annotated, Optional
+
+
+class CreateUser(SQLModel):
+    name: str
+    email: EmailStr
+    # password: str
+    password: str = Field(min_length=1, max_length=72)
+
+class UpdateUser(SQLModel):
+    name: Optional[str] = None
+    phone: Optional[int] = None
+    email: Optional[EmailStr] = None
+
+class loginUser(SQLModel):
+    email: str
+    password: str   
+
+class UserRead(SQLModel):
+    id: int
+    name: str
+    email: EmailStr
+
+class Token(SQLModel):
+    access_token: str
+    token_type: str
